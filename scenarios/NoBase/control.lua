31090c0c2ecf5c1c2c8ac3d05fef5720226429ca
Jan Mischak <jan.mischak@kstp.at>
2021-02-23 15:39:21 +0100

    Improve Base scenario and add NoBase scenario
    Remove all players from the scenario
    Remove deprecated levelPath for the debugger

--- scenarios/NoBase/control.lua
+++ scenarios/NoBase/control.lua
@@ -1,6 +1,7 @@
 
+
 if not script.active_mods["JanSharpDevEnv"] then
   error("JanSharpDevEnv is required to load this scenario.")
 end
 
-require("__JanSharpDevEnv__.scenario-scripts.Base.control")
+require("__JanSharpDevEnv__.scenario-scripts.NoBase.control")
